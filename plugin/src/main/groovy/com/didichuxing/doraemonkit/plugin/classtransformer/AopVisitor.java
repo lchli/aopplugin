f982a53ad6ec082f26c11d09145fd9f6878db6f4
lichenghang <lichenghang@babytree-inc.com>
2022-04-14 22:57:17 +0800

    f

---
+++ plugin/src/main/groovy/com/didichuxing/doraemonkit/plugin/classtransformer/AopVisitor.java
@@ -0,0 +1,56 @@
+package com.didichuxing.doraemonkit.plugin.classtransformer;
+
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.MethodVisitor;
+
+public class AopVisitor extends ClassVisitor {
+    private String className;
+
+    public AopVisitor(int api) {
+        super(api);
+    }
+
+    public AopVisitor(int api, ClassVisitor classVisitor) {
+        super(api, classVisitor);
+    }
+
+    @Override
+    public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
+        super.visit(version, access, name, signature, superName, interfaces);
+        className=name;
+
+        System.out.println("className:"+name);
+    }
+
+    @Override
+    public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
+         MethodVisitor old = super.visitMethod(access, name, descriptor, signature, exceptions);
+
+        if(className.contains("AopVisitor")){
+            return old;
+        }
+        if(className.contains("EncourageAop")){
+            return old;
+        }
+        if(className.contains("MyLocalVariablesSorter")){
+            return old;
+        }
+
+     if(className.contains("AopUtil")){
+            return old;
+        }
+        if(!className.contains("backgrounderaser")){
+            return old;
+        }
+
+        if (className.contains("thinkingdata")||className.contains("kt")
+            ||className.contains("org.jetbrains.kotlin")
+            ||className.contains("org.jetbrains.kotlinx")) {
+            return old;
+        }
+
+      return new com.didichuxing.doraemonkit.plugin.classtransformer.MyLocalVariablesSorter(api,access,descriptor,old);
+
+
+    }
+}
---
+++ plugin/src/main/groovy/com/didichuxing/doraemonkit/plugin/classtransformer/MyLocalVariablesSorter.java
@@ -0,0 +1,63 @@
+package com.didichuxing.doraemonkit.plugin.classtransformer;
+
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.Type;
+import org.objectweb.asm.commons.LocalVariablesSorter;
+
+public class MyLocalVariablesSorter extends LocalVariablesSorter {
+
+     MyLocalVariablesSorter(int access, String descriptor, MethodVisitor methodVisitor) {
+        super(access, descriptor, methodVisitor);
+    }
+
+    public MyLocalVariablesSorter(int api, int access, String descriptor, MethodVisitor methodVisitor) {
+        super(api, access, descriptor, methodVisitor);
+    }
+
+
+
+    private int time=-1;
+
+    @Override
+    public void visitCode() {
+        super.visitCode();
+        mv.visitMethodInsn(Opcodes.INVOKESTATIC, "java/lang/System", "currentTimeMillis", "()J", false);
+        time = newLocal(Type.LONG_TYPE); // 新建一个局部变量
+        mv.visitVarInsn(Opcodes.LSTORE, time);
+    }
+
+
+    @Override
+    public void visitInsn(int opcode) {
+//        if(opcode==Opcodes.ACC_SUPER){
+//           // super.visitInsn(opcode);
+//
+//            mv.visitMethodInsn(Opcodes.INVOKESTATIC, "java/lang/System", "currentTimeMillis", "()J", false);
+//            time = newLocal(Type.LONG_TYPE); // 新建一个局部变量
+//            mv.visitVarInsn(Opcodes.LSTORE, time);
+//
+//           //
+//
+//            return;
+//        }
+
+        if ((opcode >= Opcodes.IRETURN && opcode <= Opcodes.RETURN)|| Opcodes.ATHROW == opcode) {
+            mv.visitMethodInsn(Opcodes.INVOKESTATIC, "java/lang/System", "currentTimeMillis", "()J", false);
+            mv.visitVarInsn(Opcodes.LLOAD, time);
+            mv.visitInsn(Opcodes.LSUB);
+
+            mv.visitMethodInsn(Opcodes.INVOKESTATIC, "com/babytree/baf/hook/AopUtil", "log", "(J)V", false);
+            // mv.visitFieldInsn(Opcodes.GETSTATIC, owner, "timer", "J");
+            //mv.visitInsn(Opcodes.LADD);
+            // mv.visitFieldInsn(Opcodes.PUTSTATIC, owner, "timer", "J");
+        }
+        super.visitInsn(opcode);
+    }
+
+    @Override
+    public void visitMaxs(int maxStack, int maxLocals) {
+        super.visitMaxs(maxStack + 4, maxLocals);
+    }
+
+}
